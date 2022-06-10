b907934983db012d44d878830c200c67434a15ec
Jack Wang <jack610336@gmail.com>
2022-06-10 13:32:02 +0800

    test first version

--- main.py
+++ main.py
@@ -24,6 +24,11 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 moves = ['F', 'T', 'L', 'R']
+myapp="https://jack-cloud-run-hackathon-python-audhxybk5q-uc.a.run.app"
+FORWARD="FORWARD"
+THROW="THROW"
+LEFT="LEFT"
+RIGHT="RIGHT"
 
 @app.route("/", methods=['GET'])
 def index():
@@ -34,10 +39,47 @@ def move():
     request.get_data()
     logger.info(request.json)
     print("================")
-    j=json.dumps(request.json)
-    print("123123123" + j)
-    print("現在動作" + moves[3])
-    return moves[3]
+
+    objectString=json.dumps(request.json)
+    object = json.loads(objectString)
+
+    totalState=object["arena"]["state"]
+    myPos = totalState[myapp]
+
+    
+
+    return modifyPosition(myPos)
+
+
+def modifyPosition(myState):
+    x=myState["x"]
+    y=myState["y"]
+    direction=myState["direction"]
+    wasHit=myState["wasHit"]
+    score=myState["score"]
+    myInfo="My Detail Info: \nX= " + str(x) + "  \nY= " + str(y) + " \ndirection = " + direction + " \nwasHit = " + str(wasHit) + " \nscroe = " + str(score) 
+    print("✅✅✅✅✅ \n" + myInfo + "\n✅✅✅✅✅")
+
+    if wasHit:
+        return moveAction(RIGHT)    
+    else:
+        return moveAction(THROW)
+    
+
+
+
+def moveAction(status):
+    if status == FORWARD:
+        return moves[0]
+    elif status == THROW:
+        return moves[1]
+    elif status == LEFT:
+        return moves[2]
+    elif status == RIGHT:
+        return moves[3]
+    else:
+        return "ERROR"
+
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
